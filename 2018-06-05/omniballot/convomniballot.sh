baddf42b0f70de597272d8a80e21a4475e8e174e
Carl Hage <ch@carlhage.com>
2019-03-15 21:15:48 -0700

    Add 2018-06-05 data without writeins

---
+++ 2018-06-05/omniballot/convomniballot.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+../../../osv-data-converter/src/convomniballot.py $*
+#rm sha512sum.txt
+#sha512sum *.* >sha512sum.txt
---
+++ 2018-06-05/omniballot/matchcand.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# This script calls the results converter
+# It can be modified to change the path or add options
+../../../osv-data-converter/src/matchcand.py -v  $* 2>&1 |tee matchcand.log
+#rm sha256sum.txt
+#sha256sum *.* >sha256sum.txt
