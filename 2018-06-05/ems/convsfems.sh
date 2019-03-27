06ae99690b1faab08077066cb677b151e4e72b8b
Carl Hage <ch@carlhage.com>
2019-03-26 19:13:16 -0700

    Update to fix ORR compatibility issues

---
+++ 2018-06-05/ems/convsfems.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#
+../../../osv-data-converter/src/dfm-contcand.py
+../../../osv-data-converter/src/dfm-meas.py
--- 2018-06-05/omniballot/matchcand.sh
+++ 2018-06-05/omniballot/matchcand.sh
@@ -2,5 +2,4 @@
 # This script calls the results converter
 # It can be modified to change the path or add options
 ../../../osv-data-converter/src/matchcand.py -v  $* 2>&1 |tee matchcand.log
-#rm sha256sum.txt
-#sha256sum *.* >sha256sum.txt
+../../../osv-data-converter/src/matchcand.py -S .ems -v  $* 2>&1 |tee matchcand.ems.log
