2cf472580f3e6622ba076c837cdf8ff891c5ee0b
Carl Hage <ch@carlhage.com>
2019-11-11 16:24:52 -0800

    Add short_description to election.json, updated results

--- 2019-11-05/resultdata/getsfturnout.sh
+++ 2019-11-05/resultdata/getsfturnout.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-../../../osv-data-converter/src/getsfturnout.py  2019-11-05
+../../../osv-data-converter/src/getsfturnout.py -r 2019-11-05
 cd turnoutdata-raw
 rm sha256sum.txt
 sha256sum * >sha256sum.txt
