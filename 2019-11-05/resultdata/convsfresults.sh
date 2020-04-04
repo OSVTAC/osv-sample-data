0bc53f4e45e4a0b7befd3583cd24dd01284a46c1
Carl Hage <ch@carlhage.com>
2020-04-03 20:09:07 -0700

    Add extra turnout, add winning status

--- 2019-11-05/resultdata/convsfresults.sh
+++ 2019-11-05/resultdata/convsfresults.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # This script calls the results converter
 # It can be modified to change the path or add options
+convvbmprecinct.py -s
 convsfresults.py -v -P $*
 
--- 2020-03-03/ca/sf/resultdata/convsfresults.sh
+++ 2020-03-03/ca/sf/resultdata/convsfresults.sh
@@ -3,7 +3,7 @@
 # It can be modified to change the path or add options
 
 # First, preprocess the turnout data:
-convvbmprecinct.py
+convvbmprecinct.py -x
 
 # Create the ../out-orr/resultdata files:
 convsfresults.py -v -P $*
